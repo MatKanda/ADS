2507f89db495fa6235babcb31e42f1dd237fcd6e
kandy <matus.kanda.po@gmail.com>
2022-03-18 23:22:09 +0100

    feat: const and root table

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from operator import itemgetter
 
 
 TOTAL_COUNT = 0
+root_table = [[]]
+cost_table = [[]]
+
+
+def remove_end(word):
+    return word.split("\n")[0]
 
 
 def read_and_parse(input_file, output_file):
@@ -32,7 +38,6 @@ def read_and_parse(input_file, output_file):
 
 
 def lex_arrangement(input_data, output_file):
-    # https://stackoverflow.com/questions/20099669/sort-multidimensional-array-based-on-2nd-element-of-the-subarray
     # sort by word name
     input_data = sorted(input_data, key=itemgetter(1))
 
@@ -48,13 +53,14 @@ def lex_arrangement(input_data, output_file):
 def occurrence_probability(input_data, input_word):
     for i in range(len(input_data)):
         # split -> remove end of line character
-        word = input_data[i][1].split("\n")[0]
+        # word = input_data[i][1].split("\n")[0]
+        word = remove_end(input_data[i][1])
         if word == input_word:
             value = int(input_data[i][0])
             return value/TOTAL_COUNT
 
 
-def find_optional_root(input_data):
+def find_best_root(input_data):
     """
     Find best tree root for data from "parsed_dictionary.txt"
     """
@@ -68,7 +74,7 @@ def find_optional_root(input_data):
             cost = 0
             for j in range(len(input_data)):
                 # split -> remove end of line character
-                word = input_data[j][1].split("\n")[0]
+                word = remove_end(input_data[j][1])
                 # occurrence * level (depth)
                 current_cost = occurrence_probability(input_data, word) * level
                 cost += current_cost
@@ -80,7 +86,7 @@ def find_optional_root(input_data):
             cost = 0
             for j in reversed(range(len(input_data))):
                 # split -> remove end of line character
-                word = input_data[j][1].split("\n")[0]
+                word = remove_end(input_data[j][1])
                 # occurrence * level (depth)
                 current_cost = occurrence_probability(input_data, word) * level
                 cost += current_cost
@@ -92,7 +98,7 @@ def find_optional_root(input_data):
             cost = 0
 
             # compute root
-            word = input_data[i][1].split("\n")[0]
+            word = remove_end(input_data[i][1])
             # occurrence * level (depth)
             current_cost = occurrence_probability(input_data, word) * level
             cost += current_cost
@@ -101,7 +107,7 @@ def find_optional_root(input_data):
             level = 2
             for j in reversed(range(0, i)):
                 # split -> remove end of line character
-                word = input_data[j][1].split("\n")[0]
+                word = remove_end(input_data[j][1])
                 # occurrence * level (depth)
                 current_cost = occurrence_probability(input_data, word) * level
                 cost += current_cost
@@ -111,7 +117,7 @@ def find_optional_root(input_data):
             level = 2
             for j in range(i+1, len(input_data)):
                 # split -> remove end of line character
-                word = input_data[j][1].split("\n")[0]
+                word = remove_end(input_data[j][1])
                 # occurrence * level (depth)
                 current_cost = occurrence_probability(input_data, word) * level
                 cost += current_cost
@@ -120,7 +126,43 @@ def find_optional_root(input_data):
 
     print(results)
     index = results.index(min(results))
+    print(f"Best root word is: {input_data[index][1]}Cost is :{results[index]}")
     return input_data[index][1]
+
+
+def increasing_depth(start, end, input_data):
+    count = 0
+    for i in range(start, end):
+        count += occurrence_probability(input_data, remove_end(input_data[i][1]))
+    return count
+
+
+def create_cost_and_root_table(input_data):
+    global root_table
+    global cost_table
+    n = len(input_data)
+    root_table = [[0 for i in range(n)] for i in range(n)]
+    cost_table = [[0 for i in range(n)] for i in range(n)]
+
+    # init
+    for i in range(1, n):
+        cost_table[i][i] = occurrence_probability(input_data, remove_end(input_data[i][1]))
+        root_table[i][i] = i
+
+    for d in range(1, n-1):
+        for i in range(n-d):
+            j = d + i
+            _min = float('inf')
+            tmp = 0
+            for l in range(i, j):
+                # combination of left subtree + right subtree
+                tmp = cost_table[i][l - 1] + cost_table[l + 1][j]
+                # new min, update
+                if tmp < _min:
+                    _min = tmp
+                    # save root that gave us min value
+                    root_table[i][j] = l
+            cost_table[i][j] = tmp + increasing_depth(i, j, input_data)
 
 
 if __name__ == "__main__":
@@ -128,5 +170,6 @@ if __name__ == "__main__":
     print(f"Total count of all values is: {TOTAL_COUNT}")
     probability = occurrence_probability(data, "the")
     print(probability)
-    # print(data)
-    print(f"Best roof is: {find_optional_root(data)}")
+    root = find_best_root(data)
+    create_cost_and_root_table(data)
+    print(cost_table)
