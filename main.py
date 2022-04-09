0bb26ad2d03c7b2dfd8203d60e2550ae26d0f9d6
kandy <matus.kanda.po@gmail.com>
2022-04-09 09:41:53 +0200

    feat: edit-distance method

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from load_file import *
 from LCS import *
-
+from ED import *
 
 if __name__ == "__main__":
     input_text_incorrect = load_text("vstup_nespravny.txt")
     input_text_correct = load_text("vstup_spravny.txt")
     dictionary = load_dict("slovnik.txt")
 
-    data = correct_text(input_text_incorrect, dictionary)
+    print(f"Correct sentence is:\n{input_text_correct}\n")
+
+    # 1. option - subsequence
+    data = correct_text_1(input_text_incorrect, dictionary, is_subsequence=True)
     print(data)
-    print(f"Number of incorrect words: {compare_results(input_text_correct, data)}")
+    print(f"Number of incorrect words in subsequence method: {compare_results(input_text_correct, data)}\n")
+
+    # 2. option - substring
+    data = correct_text_1(input_text_incorrect, dictionary, is_subsequence=False)
+    print(data)
+    print(f"Number of incorrect words in substring method: {compare_results(input_text_correct, data)}\n")
+
+    # 3. option - edit distance
+    data = correct_text_2(input_text_incorrect, dictionary)
+    print(data)
+    print(f"Number of incorrect words in distance method: {compare_results(input_text_correct, data)}\n")
